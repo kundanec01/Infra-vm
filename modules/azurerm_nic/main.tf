f8eaa63ff541eb984e22b246d3ca313acd22e00e
kundan singh <kundanec01@gmail.com>
2026-01-24 21:42:18 +0530

    new vm5

--- modules/azurerm_nic/main.tf
+++ modules/azurerm_nic/main.tf
@@ -5,7 +5,7 @@ resource "azurerm_network_interface" "nic" {
 
   ip_configuration {
     name                          = "testconfiguration1"
-    subnet_id                     = "/subscriptions/c46beff3-d22c-4a6f-82a7-693fd94c69c4/resourceGroups/kundan-rg/providers/Microsoft.Network/virtualNetworks/kk-vnet/subnets/kk-subnet"
+    subnet_id                     = ["/subscriptions/c46beff3-d22c-4a6f-82a7-693fd94c69c4/resourceGroups/kundan-rg/providers/Microsoft.Network/virtualNetworks/kk-vnet/subnets/kk-subnet"]
     private_ip_address_allocation = "Dynamic"
   }
 }
